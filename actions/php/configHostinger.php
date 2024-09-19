f8acb6fa836fe16895e8e7e7693b7fb58c2268ca
Jannatul Fardus Muslim <jannatul.fardus@hotmail.com>
2024-09-19 21:59:44 +0100

    created config for hostinger

---
+++ actions/php/configHostinger.php
@@ -0,0 +1,15 @@
+
+<?php
+
+	// connection details for MySQL database
+
+	$cd_host = "127.0.0.1";
+	$cd_port = 3306;
+	$cd_socket = "";
+
+	// database name, username and password
+	$bloodbank_dbname = "u545091865_blood_bank";
+	$bloodbank_user = "u545091865_blood_bank";
+	$bloodbank_password = "5U[v&/ZAc#M";
+
+?>
