085e532a5b1be452898724091f41d81c85db3282
Jannatul Fardus Muslim <jannatul.fardus@hotmail.com>
2024-09-26 13:48:04 +0100

    corrected follwing these : db username and signup form action path.

--- actions/php/configHostinger.php
+++ actions/php/configHostinger.php
@@ -1,15 +1,22 @@
-
 <?php
+    class Database {
+    private $host = '127.0.0.1'; // Hostinger Database host (e.g., 'localhost' or specific hostname)
+    private $db_name = 'u545091865_bloodbank'; // Hostinger Database name
+    private $username = 'u545091865_blood_bank'; // Hostinger Database username
+    private $password = '5U[v&/ZAc#M'; // Hostinger Database password
+    public $conn;
 
-	// connection details for MySQL database
-
-	$cd_host = "127.0.0.1";
-	$cd_port = 3306;
-	$cd_socket = "";
-
-	// database name, username and password
-	$bloodbank_dbname = "u545091865_blood_bank";
-	$bloodbank_user = "u545091865_blood_bank";
-	$bloodbank_password = "5U[v&/ZAc#M";
-
+    // Method to get the database connection
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+    
+    }
 ?>
